f31d8ad3ed32b16254031018122fdf98a6482e34
juandouglaspe <92160838+juandouglaspe@users.noreply.github.com>
2021-10-23 21:13:01 -0300

    Get Authorization

---
+++ Nexus.Release.Manager/Nexus.Github.Client/OAuth.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Net;
+using System.Web;
+
+namespace Nexus.Github.Client;
+public class OAuth
+{
+    public string ClientId { get; set; }
+    public string ClientSecret { get; set; }
+    private readonly string requestUrl;
+    private const string htmlClose = "<!DOCTYPE html><html><head></head><body onload=\"window.close()\">You can now close this page!</body></html>";
+    public string UserCode { get; set; }
+    public OAuth(string clientId, string clientSecret)
+    {
+        ClientId = clientId;
+        ClientSecret = clientSecret;
+
+        requestUrl = $"https://github.com/login/oauth/authorize?client_id={ClientId}";
+    }
+
+    public async Task RequestLoginAsync(TimeSpan maxAwait)
+    {
+        Task awaitResponse = Task.Run(() =>
+        {
+            AwaitResponse("http://localhost:1337/", htmlClose);
+        });
+
+        ProcessStartInfo info = new(requestUrl);
+        info.UseShellExecute = true;
+
+        Process.Start(info);
+        await awaitResponse.WaitAsync(maxAwait);
+    }
+
+    private void AwaitResponse(string prefix, string serverResponse)
+    {
+        bool authenticated = false;
+        HttpListener listener = new();
+        listener.Prefixes.Add(prefix);
+        listener.Start();
+
+        while (!authenticated)
+        {
+            HttpListenerContext context = listener.GetContext();
+            HttpListenerRequest request = context.Request;
+            HttpListenerResponse response = context.Response;
+
+
+            //
+            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(serverResponse);
+            response.ContentLength64 = buffer.Length;
+            Stream output = response.OutputStream;
+            output.Write(buffer, 0, buffer.Length);
+            output.Close();
+
+            Uri? uri = request.Url;
+
+            if (uri == null)
+                break;
+
+            if (uri.LocalPath != "/Authentication/Github/Callback")
+                break;
+
+            NameValueCollection queryCollection = HttpUtility.ParseQueryString(uri.Query);
+
+            if (queryCollection == null)
+                break;
+
+            string code = queryCollection["code"];
+
+            if (code == null)
+                break;
+
+            UserCode = code;
+            authenticated = true;
+        }
+
+        listener.Stop();
+    }
+}
---
+++ Nexus.Release.Manager/Nexus.Release.Manager/Program.cs
@@ -0,0 +1,24 @@
+﻿using Nexus.Github.Client;
+using System;
+using System.Diagnostics;
+using System.Net;
+
+namespace Nexus.Release.Manager
+{
+    public class Program
+    {
+        static OAuth auth;
+        const string ClientId = "12b0da7399309b6c911c";
+        const string ClientSecret = "008926850e5ed94a0a988268153e87a9767f79df";
+
+        public static void Main(string[] args)
+        {
+            auth = new(ClientId, ClientSecret);
+            Console.WriteLine();
+
+            auth.RequestLoginAsync(TimeSpan.FromMinutes(5))
+                   .Wait();
+            Console.ReadLine();
+        }
+    }
+}
